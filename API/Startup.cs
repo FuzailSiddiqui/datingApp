89fa8fd3e8921c13c4347e5c11f0e8532cbf7f5f
fuzailsiddiqui <thefuzail@hotmail.com>
2020-11-09 01:04:30 +0400

    Section 3 finiished - Second Commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +47,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=>x.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
